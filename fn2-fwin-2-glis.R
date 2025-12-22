675f0bcf322694ef96cbd900f7bb7188e03ff0d2
holdenje <jeremy.holden@ontario.ca>
2025-12-22 11:56:55 -0500

    Adds FN012 import code

--- fn2-fwin-2-glis.R
+++ fn2-fwin-2-glis.R
@@ -1,3 +1,4 @@
+# Libraries
 library(foreign)
 library(dplyr)
 library(readxl)
@@ -6,10 +7,13 @@ library(ggplot2)
 library(stringr)
 library(RODBC)
 library(gfsR)
+library(readr)
 
+# FN2 Files
 allfiles <- dir("FN2Data/45D/Ia01_STO", recursive = T, full.names = T)
 dbffiles <- allfiles[str_detect(allfiles, pattern = "DBF$")]
 
+# GL Template 5 table names
 dbase_template <- "Great_Lakes_Assessment_Template_5.accdb"
 conn_template <- odbcConnectAccess2007(dbase_template, uid = "", pwd = "")
 fn011_names <- sqlColumns(conn_template, "FN011")$COLUMN_NAME
@@ -37,14 +41,27 @@ FN011 <- FN011 %>%
   
 FN011 <- FN011 %>% select(all_of(fn011_names))
 
-dbase_write <- file.path("TemplatedData", paste0(FN011$PRJ_CD, "_T5.accdb"))
-if(file.exists(dbase_write)) {file.remove(dbase_write)}
-file.copy(dbase_template, dbase_write)
-conn_write <- odbcConnectAccess2007(dbase_write, uid = "", pwd = "")
-isverbose = FALSE
-sqlSave(conn_write, FN011, tablename = "FN011", append = TRUE, rownames = FALSE, verbose = isverbose)
-odbcClose(conn_write)
-
 # FN012
 FN012 <- read.dbf(dbffiles[str_detect(dbffiles, pattern = "FN012")])
-setdiff(fn012_names, names(FN012))
+FN012_glfishr <- read_csv("FN012_ALL.csv")
+setdiff(names(FN012_glfishr), names(FN012))
+FN012 <- left_join(FN012, FN012_glfishr)
+setdiff(fn012_names, names(FN012)) # TISSUE, AGEST, LAMSAM
+FN012 <- FN012 %>% 
+  mutate(TISSUE = 1, AGEST = 1, LAMSAM = 0)
+setdiff(fn012_names, names(FN012)) # should match now
+names(FN012) == fn012_names
+names(FN012)
+fn012_names
+FN012 <- FN012 %>% select(all_of(fn012_names))
+
+# Create T5 data base
+dbase_write <- file.path("TemplatedData", paste0(FN011$PRJ_CD, "_T5.accdb"))
+if(file.exists(dbase_write)) {file.remove(dbase_write)} # remove any previous versions
+file.copy(dbase_template, dbase_write) # write blank database
+
+conn_write <- odbcConnectAccess2007(dbase_write, uid = "", pwd = "")
+isverbose = TRUE
+sqlSave(conn_write, FN011, tablename = "FN011", append = TRUE, rownames = FALSE, verbose = isverbose)
+sqlSave(conn_write, FN012, tablename = "FN012", append = TRUE, rownames = FALSE, verbose = isverbose)
+odbcClose(conn_write)
